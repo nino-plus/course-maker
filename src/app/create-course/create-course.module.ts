06810a7c0d6141c1a096a44544f24bb0c0c36e45
shhymmt <mail@shhymmt.com>
2021-01-15 15:57:34 +0900

    GoogleMapsApiをモジュールに追加

--- src/app/create-course/create-course.module.ts
+++ src/app/create-course/create-course.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CreateCourseRoutingModule } from './create-course-routing.module';
 import { CreateCourseComponent } from './create-course/create-course.component';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [CreateCourseComponent],
-  imports: [
-    CommonModule,
-    CreateCourseRoutingModule
-  ]
+  imports: [CommonModule, CreateCourseRoutingModule, GoogleMapsModule],
 })
-export class CreateCourseModule { }
+export class CreateCourseModule {}
--- src/app/play-course/play-course.module.ts
+++ src/app/play-course/play-course.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PlayCourseRoutingModule } from './play-course-routing.module';
 import { PlayCourseComponent } from './play-course/play-course.component';
 import { ResultDialogComponent } from './result-dialog/result-dialog.component';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [PlayCourseComponent, ResultDialogComponent],
-  imports: [
-    CommonModule,
-    PlayCourseRoutingModule
-  ]
+  imports: [CommonModule, PlayCourseRoutingModule, GoogleMapsModule],
 })
-export class PlayCourseModule { }
+export class PlayCourseModule {}
