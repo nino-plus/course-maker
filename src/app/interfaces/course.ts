ee694839830c30e2708eb45439f6d45c89b6e1c5
shhymmt <mail@shhymmt.com>
2021-01-15 11:19:13 +0900

    interfaceに初期値を追加

--- src/app/interfaces/course.ts
+++ src/app/interfaces/course.ts
@@ -1,2 +1,7 @@
 export interface Course {
+  courseId: string;
+  creatorId: string;
+  createdAt: Date;
+  playCounts: number;
+  clearUserCounts: number;
 }
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,2 +1,6 @@
 export interface User {
+  uid: string;
+  displayName: string;
+  avatarUrl: string;
+  createdAt: Date;
 }
