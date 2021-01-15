dd4ae5cd6a71ecbd4ed0750a94b18b243dbcb44e
shhymmt <mail@shhymmt.com>
2021-01-15 15:28:44 +0900

    githubaction environmentの設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCyYYbzDKkIlE7d_bB5kgK3XV4HPaT-dRc',
+    authDomain: 'course-maker-fb177.firebaseapp.com',
+    projectId: 'course-maker-fb177',
+    storageBucket: 'course-maker-fb177.appspot.com',
+    messagingSenderId: '262423974076',
+    appId: '1:262423974076:web:ad1bb7104ebe5e0a2fc0b9',
+    measurementId: 'G-TTKPCBPYKH'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCyYYbzDKkIlE7d_bB5kgK3XV4HPaT-dRc',
+    authDomain: 'course-maker-fb177.firebaseapp.com',
+    projectId: 'course-maker-fb177',
+    storageBucket: 'course-maker-fb177.appspot.com',
+    messagingSenderId: '262423974076',
+    appId: '1:262423974076:web:ad1bb7104ebe5e0a2fc0b9',
+    measurementId: 'G-TTKPCBPYKH'
+  }
 };
 
 /*
