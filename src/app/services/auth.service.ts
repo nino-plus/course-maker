53d771127e99c0796dadd34e698cf9091f833372
shhymmt <mail@shhymmt.com>
2021-01-16 13:11:56 +0900

    auth.serviceをプルした

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
+import { Observable, of } from 'rxjs';
 import { User } from '../interfaces/user';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  user$: Observable<User>;
+  uid: string;
 
-  constructor() {}
+  user$: Observable<User> = this.afAuth.authState.pipe(
+    switchMap((afUser) => {
+      if (afUser) {
+        this.uid = afUser.uid;
+        return this.db.doc<User>(`users/${afUser.uid}`).valueChanges();
+      } else {
+        return of(null);
+      }
+    })
+  );
+  constructor(
+    public afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {}
+
+  login() {
+    const provider = new firebase.auth.TwitterAuthProvider();
+    provider.setCustomParameters({ prompt: 'select_account' });
+    this.afAuth.signInWithPopup(provider);
+  }
+
+  logout() {
+    this.afAuth.signOut().then(() => this.router.navigateByUrl('/welcome'));
+  }
 }
