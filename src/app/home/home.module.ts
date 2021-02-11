eb365860ef0650b060c96707847b36d93b6bc517
hide-a1 <hide210g@gmail.com>
2021-02-11 12:44:04 +0900

    feat:【Home】コース一覧を無限スクロールに対応 Fixes #84

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule,
+    InfiniteScrollModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class HomeModule {}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,17 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Observable, of, Subscription } from 'rxjs';
+import { take } from 'rxjs/internal/operators/take';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { Course } from 'src/app/interfaces/course';
+import { User } from 'src/app/interfaces/user';
 import { CourseService } from 'src/app/services/course.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   courses$: Observable<Course[]> = this.courseService.getCourses();
+  courses: Course[] = [];
+  isComplete: boolean;
+  isLoading: boolean;
+  subscription = new Subscription();
+  prevLastDoc;
+  lastDoc;
 
-  constructor(private courseService: CourseService) {}
+  constructor(
+    private courseService: CourseService,
+    private userService: UserService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  getCourses() {
+    if (this.isComplete || this.isLoading) {
+      return;
+    }
+    this.isLoading = true;
+    setTimeout(() => {
+      this.courseService
+        .getCoursesForInfinitScroll(this.lastDoc)
+        .pipe(
+          take(1),
+          tap((docs) => {
+            if (docs) {
+              this.lastDoc = docs[docs.length - 1];
+            }
+          }),
+          map((snaps) => {
+            return snaps.map((snap) => snap.data() as Course);
+          }),
+          switchMap((courses: Course[]) => {
+            const creatorIds: string[] = Array.from(
+              new Set(
+                courses.map((course) => {
+                  return course.creatorId;
+                })
+              )
+            );
+            const creators$: Observable<User[]> = combineLatest(
+              creatorIds.map((id) => {
+                return this.userService.getUser(id);
+              })
+            );
+            return combineLatest([of(courses), creators$]);
+          }),
+          map(([courses, creators]) => {
+            return courses.map((course) => {
+              return {
+                ...course,
+                user: creators.find(
+                  (creator) => course.creatorId === creator.uid
+                ),
+              };
+            });
+          })
+        )
+        .subscribe((docs) => {
+          if (docs) {
+            if (!docs.length) {
+              this.isComplete = true;
+              return;
+            }
+            console.log('compare:', docs);
+            // 最初の取得が３回coursesにプッシュされてしまうので、同じ配列の場合プッシュしないように条件式を設定
+            if (JSON.stringify(docs) !== JSON.stringify(this.courses)) {
+              this.courses.push(...docs);
+              console.log(this.courses);
+            }
+          }
+        });
+      this.isLoading = false;
+    }, 500);
+  }
 }
